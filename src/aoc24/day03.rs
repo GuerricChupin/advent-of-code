c489326bb6064a5974149f878fbc480dbd3341c3
Guerric Chupin <guerric.chupin@gmail.com>
2024-12-03 06:45:14 +0100

    Day 03

--- src/aoc24/day03.rs
+++ src/aoc24/day03.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 use crate::puzzle::Puzzle;
 
 pub struct Day03 {
@@ -12,10 +14,36 @@ impl Puzzle for Day03 {
     }
 
     fn part1(self) -> Option<i64> {
-        None
+        let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+        Some(
+            re.captures_iter(&self.input)
+                .map(|capture| {
+                    capture[1].parse::<i64>().unwrap() * capture[2].parse::<i64>().unwrap()
+                })
+                .sum(),
+        )
     }
 
     fn part2(self) -> Option<i64> {
-        None
+        let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+
+        let mut enabled = true;
+        let mut total = 0;
+
+        for capture in re.captures_iter(&self.input) {
+            match &capture[0] {
+                "do()" => enabled = true,
+                "don't()" => enabled = false,
+                _ => {
+                    if enabled {
+                        total +=
+                            capture[1].parse::<i64>().unwrap() * capture[2].parse::<i64>().unwrap();
+                    }
+                }
+            }
+        }
+
+        Some(total)
     }
 }
