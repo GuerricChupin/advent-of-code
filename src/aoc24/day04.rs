5f6e9f49a29d4e2c69fc1dc4b5cf0b98f3eac512
Guerric Chupin <guerric.chupin@gmail.com>
2024-12-04 12:28:13 +0100

    Day 4

---
+++ src/aoc24/day04.rs
@@ -0,0 +1,92 @@
+use crate::puzzle::Puzzle;
+
+fn check_word_against_option<const N: usize>(
+    input: &Vec<Vec<u8>>,
+    option: [(usize, usize); N],
+    possible_words: &[[u8; N]],
+) -> bool {
+    possible_words.into_iter().any(|word| {
+        word.into_iter().zip(option).all(|(c, (i, j))| {
+            input
+                .get(i)
+                .and_then(|line| line.get(j))
+                .map(|k| k == c)
+                .unwrap_or(false)
+        })
+    })
+}
+
+fn check_xmas(input: &Vec<Vec<u8>>, i: usize, j: usize) -> i64 {
+    let horizontal = [(i, j), (i + 1, j), (i + 2, j), (i + 3, j)];
+    let vertical = [(i, j), (i, j + 1), (i, j + 2), (i, j + 3)];
+    let south_east_diagonal = [(i, j), (i + 1, j + 1), (i + 2, j + 2), (i + 3, j + 3)];
+    let north_east_diagonal = [(i, j + 3), (i + 1, j + 2), (i + 2, j + 1), (i + 3, j)];
+
+    let options = [
+        horizontal,
+        vertical,
+        south_east_diagonal,
+        north_east_diagonal,
+    ];
+
+    let words = [*b"XMAS", *b"SAMX"];
+
+    options
+        .into_iter()
+        .filter(|&option| check_word_against_option(input, option, &words))
+        .count() as i64
+}
+
+fn check_mas(input: &Vec<Vec<u8>>, i: usize, j: usize) -> bool {
+    let south_east_diagonal = [(i, j), (i + 1, j + 1), (i + 2, j + 2)];
+    let north_east_diagonal = [(i, j + 2), (i + 1, j + 1), (i + 2, j)];
+
+    let options = [south_east_diagonal, north_east_diagonal];
+
+    let words = [*b"MAS", *b"SAM"];
+
+    options
+        .into_iter()
+        .all(|option| check_word_against_option(input, option, &words))
+}
+
+pub struct Day04 {
+    input: Vec<Vec<u8>>,
+}
+
+impl Puzzle for Day04 {
+    fn parse(input: &str) -> Option<Self> {
+        Some(Day04 {
+            input: input
+                .lines()
+                .map(|line| Vec::from(line.as_bytes()))
+                .collect::<Vec<_>>(),
+        })
+    }
+
+    fn part1(self) -> Option<i64> {
+        let mut count = 0;
+
+        for (j, line) in self.input.iter().enumerate() {
+            for (i, _) in line.iter().enumerate() {
+                count += check_xmas(&self.input, i, j)
+            }
+        }
+
+        Some(count)
+    }
+
+    fn part2(self) -> Option<i64> {
+        let mut count = 0;
+
+        for (j, line) in self.input.iter().enumerate() {
+            for (i, _) in line.iter().enumerate() {
+                if check_mas(&self.input, i, j) {
+                    count += 1
+                }
+            }
+        }
+
+        Some(count)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod aoc24 {
     pub mod day01;
     pub mod day02;
     pub mod day03;
+    pub mod day04;
 }
 
 use anyhow::anyhow;
@@ -31,6 +32,7 @@ fn puzzle(year: i32, day: u32, part: i64, input: &str) -> Option<i64> {
         (2024, 01) => aoc24::day01::Day01::parse(input)?.part(part),
         (2024, 02) => aoc24::day02::Day02::parse(input)?.part(part),
         (2024, 03) => aoc24::day03::Day03::parse(input)?.part(part),
+        (2024, 04) => aoc24::day04::Day04::parse(input)?.part(part),
         _ => None,
     }
 }
