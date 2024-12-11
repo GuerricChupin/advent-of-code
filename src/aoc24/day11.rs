6a05f8c2a12d2d16fc43415efbe1cbed964fb61f
Guerric Chupin <guerric.chupin@gmail.com>
2024-12-11 16:28:19 +0100

    Day 11 part 2

--- src/aoc24/day11.rs
+++ src/aoc24/day11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::puzzle::Puzzle;
 
 fn step_stone(stone: i64) -> (i64, Option<i64>) {
@@ -7,8 +9,9 @@ fn step_stone(stone: i64) -> (i64, Option<i64>) {
         let digits = 1 + stone.ilog10();
 
         if digits % 2 == 0 {
-            let first_stone = stone / (10_i64.pow(digits / 2));
-            let second_stone = stone % 10_i64.pow(digits / 2);
+            let divisor = 10_i64.pow(digits / 2);
+            let first_stone = stone / divisor;
+            let second_stone = stone % divisor;
             (first_stone, Some(second_stone))
         } else {
             (stone * 2_024, None)
@@ -16,19 +19,43 @@ fn step_stone(stone: i64) -> (i64, Option<i64>) {
     }
 }
 
-fn step_all_stones(stones: &[i64]) -> Vec<i64> {
-    let mut new_stones = Vec::with_capacity(stones.len());
+fn step_all_stones(stones: HashMap<i64, i64>) -> HashMap<i64, i64> {
+    let mut new_stones = HashMap::with_capacity(stones.len());
 
-    for &stone in stones { 
+    for (stone, stone_count) in stones.into_iter() {
         let (first_stone, optional_second_stone) = step_stone(stone);
-        new_stones.push(first_stone);
+        *new_stones.entry(first_stone).or_insert(0) += stone_count;
 
-        if let Some(second_stone) = optional_second_stone { 
-            new_stones.push(second_stone);
+        if let Some(second_stone) = optional_second_stone {
+            *new_stones.entry(second_stone).or_insert(0) += stone_count;
         }
     }
 
     new_stones
+}
+
+fn make_original_stone_map(stones: Vec<i64>) -> HashMap<i64, i64> {
+    let mut stone_map = HashMap::with_capacity(stones.len());
+
+    for stone in stones.into_iter() {
+        *stone_map.entry(stone).or_insert(0) += 1;
+    }
+
+    stone_map
+}
+
+fn count_all_stones(stones: HashMap<i64, i64>) -> i64 {
+    stones.into_iter().map(|(_, stone_count)| stone_count).sum()
+}
+
+fn count_stones_after_steps(stones: Vec<i64>, step_count: u32) -> i64 {
+    let mut stones = make_original_stone_map(stones);
+
+    for _step in 0..step_count {
+        stones = step_all_stones(stones);
+    }
+
+    count_all_stones(stones)
 }
 
 pub struct Day11 {
@@ -45,15 +72,11 @@ impl Puzzle for Day11 {
         })
     }
 
-    fn part1(mut self) -> Option<i64> {
-        for _step in 0..25 {
-            self.stones = step_all_stones(&self.stones);
-        }
-
-        Some(self.stones.len() as i64)
+    fn part1(self) -> Option<i64> {
+        Some(count_stones_after_steps(self.stones, 25))
     }
 
     fn part2(self) -> Option<i64> {
-        None
+        Some(count_stones_after_steps(self.stones, 75))
     }
 }
