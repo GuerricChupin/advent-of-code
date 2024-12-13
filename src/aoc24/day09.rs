f80da2679a77a19fbed6bebd361b10a8cd4e53c5
Guerric Chupin <guerric.chupin@gmail.com>
2024-12-13 17:09:33 +0100

    Fixed day 9 part 2
    Same solution for part 1 and 2, that makes part 1
    very slow though (~20 seconds)

--- src/aoc24/day09.rs
+++ src/aoc24/day09.rs
@@ -1,73 +1,149 @@
-use std::{collections::VecDeque, iter::repeat, num::NonZeroUsize};
-
 use crate::puzzle::Puzzle;
 
 #[derive(Debug, Clone, Copy)]
-enum Block {
-    Free(NonZeroUsize),
-    Occupied(NonZeroUsize, i64),
+struct Occupied {
+    size: usize,
+    id: i64,
 }
 
-fn defragment(disk: &mut Vec<i64>, mut blocks: VecDeque<Block>) {
-    loop {
-        let first_block = blocks.pop_front();
-        let last_block = blocks.pop_back();
+#[derive(Debug, Clone, Copy)]
+enum Block {
+    Free(usize),
+    Occupied(Occupied, bool),
+}
 
-        match (first_block, last_block) {
-            (None, Some(_)) => unreachable!(),
-            (None, None) => return,
-            (Some(Block::Occupied(block_size, block_id)), _) => {
-                disk.extend(repeat(block_id).take(block_size.into()));
+fn fit_block_into(blocks: &[Block], block: Occupied) -> Vec<Block> {
+    let mut new_blocks = Vec::with_capacity(blocks.len() + 1);
+    let mut found_slot = false;
 
-                if let Some(block) = last_block {
-                    blocks.push_back(block);
+    for slot in blocks.iter() {
+        match *slot {
+            Block::Free(free_space) if !found_slot && free_space >= block.size => {
+                let new_free_space = free_space - block.size;
+                new_blocks.push(Block::Occupied(block, true));
+                if new_free_space > 0 {
+                    new_blocks.push(Block::Free(new_free_space))
                 }
+
+                found_slot = true;
             }
-            (_, Some(Block::Free(_))) | (Some(Block::Free(_)), None) => {
-                if let Some(block) = first_block {
-                    blocks.push_front(block);
-                }
+            slot @ (Block::Occupied(_, _) | Block::Free(_)) => {
+                new_blocks.push(slot);
             }
-            (Some(Block::Free(free_space)), Some(Block::Occupied(block_size, block_id))) => {
-                let bytes_to_copy = free_space.min(block_size).into();
+        }
+    }
 
-                disk.extend(repeat(block_id).take(bytes_to_copy));
+    if !found_slot {
+        new_blocks.push(Block::Occupied(block, true))
+    }
 
-                let new_free_space = NonZeroUsize::new(usize::from(free_space) - bytes_to_copy);
-                let new_block_size = NonZeroUsize::new(usize::from(block_size) - bytes_to_copy);
+    new_blocks
+}
 
-                if let Some(new_free_space) = new_free_space {
-                    blocks.push_front(Block::Free(new_free_space));
-                }
+fn merge_free_space(blocks: &[Block]) -> Vec<Block> {
+    let mut prefix_free_space: Option<usize> = None;
 
-                if let Some(new_block_size) = new_block_size {
-                    blocks.push_back(Block::Occupied(new_block_size, block_id));
+    let mut new_block = Vec::new();
+
+    for slot in blocks {
+        match (prefix_free_space, slot) {
+            (Some(prefix_space), Block::Free(free_space)) => {
+                prefix_free_space = Some(prefix_space + *free_space);
+            }
+            (None, Block::Free(free_space)) => {
+                prefix_free_space = Some(*free_space);
+            }
+            (Some(prefix_space), Block::Occupied(occupied, pinned)) => {
+                new_block.push(Block::Free(prefix_space));
+                new_block.push(Block::Occupied(*occupied, *pinned));
+                prefix_free_space = None;
+            }
+            (None, Block::Occupied(occupied, pinned)) => {
+                new_block.push(Block::Occupied(*occupied, *pinned));
+            }
+        }
+    }
+
+    new_block
+}
+
+fn defragment_by_block(blocks: &[Block]) -> Vec<Block> {
+    let mut blocks = Vec::from(blocks);
+
+    let mut there_are_unpinned_blocks = true;
+
+    while there_are_unpinned_blocks {
+        there_are_unpinned_blocks = false;
+
+        'search_unpinned: for block in blocks.iter_mut().rev() {
+            match block {
+                Block::Occupied(_, true) | Block::Free(_) => (),
+                Block::Occupied(occupied, false) => {
+                    let to_be_inserted = *occupied;
+                    *block = Block::Free(occupied.size);
+                    blocks = fit_block_into(&blocks, to_be_inserted);
+                    there_are_unpinned_blocks = true;
+                    break 'search_unpinned;
                 }
             }
         }
     }
+
+    merge_free_space(&blocks)
 }
 
-fn to_block(disk_map: &[usize]) -> VecDeque<Block> {
+fn checksum(blocks: &[Block]) -> i64 {
+    let mut current_index = 0;
+    let mut checksum = 0;
+
+    for block in blocks {
+        match block {
+            Block::Free(free_space) => {
+                current_index += *free_space as i64;
+            }
+            Block::Occupied(Occupied { size, id }, _) => {
+                for _sub_addr in 0..*size {
+                    checksum += current_index * *id;
+                    current_index += 1;
+                }
+            }
+        }
+    }
+
+    checksum
+}
+
+fn to_block(disk_map: &[usize]) -> Vec<Block> {
     disk_map
         .iter()
         .enumerate()
         .filter_map(|(i, block_size)| {
-            let block_size = NonZeroUsize::new(*block_size)?;
             if i % 2 == 0 {
-                Some(Block::Occupied(block_size, (i / 2) as i64))
+                Some(Block::Occupied(
+                    Occupied {
+                        size: *block_size,
+                        id: (i / 2) as i64,
+                    },
+                    false,
+                ))
             } else {
-                Some(Block::Free(block_size))
+                Some(Block::Free(*block_size))
             }
         })
-        .collect::<VecDeque<_>>()
+        .collect::<Vec<_>>()
 }
 
-fn checksum(disk: &[i64]) -> i64 {
-    disk.iter()
-        .enumerate()
-        .map(|(i, value)| i as i64 * value)
-        .sum()
+fn flatten_blocks(blocks: Vec<Block>) -> Vec<Block> {
+    blocks
+        .into_iter()
+        .map(|block| match block {
+            Block::Free(free) => vec![Block::Free(free)],
+            Block::Occupied(Occupied { size, id }, pinned) => {
+                vec![Block::Occupied(Occupied { size: 1, id }, pinned); size]
+            }
+        })
+        .flatten()
+        .collect()
 }
 
 pub struct Day09 {
@@ -86,13 +162,14 @@ impl Puzzle for Day09 {
 
     fn part1(self) -> Option<i64> {
         let blocks = to_block(&self.disk_map);
-        let mut disk = Vec::new();
-        defragment(&mut disk, blocks);
-
-        Some(checksum(&disk))
+        let blocks = flatten_blocks(blocks);
+        let defragmented_blocks = defragment_by_block(&blocks);
+        Some(checksum(&defragmented_blocks))
     }
 
     fn part2(self) -> Option<i64> {
-        None
+        let blocks = to_block(&self.disk_map);
+        let defragmented_blocks = defragment_by_block(&blocks);
+        Some(checksum(&defragmented_blocks))
     }
 }
